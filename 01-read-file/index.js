f99e23ef4c75e87e828ad60359d4c5b835e2b750
Emiliya <emilya.korkunova@gmail.com>
2022-11-04 16:41:32 +0300

    fix: fix 01 solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 let filePаth = path.join(__dirname, 'text.txt');
-let stream = new fs.ReadStream(filePаth, {encoding: 'utf-8'});
+let stream = fs.createReadStream(filePаth, {encoding: 'utf-8'});
 
 stream.on('readable', function(){
   let data = stream.read();
